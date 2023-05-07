3094dbe5ddd015900cfaad5708e7d4cf432ade5d
Sean Lynch <techniq35@gmail.com>
2023-05-07 19:44:07 -0400

    Fix build when using `pg.Pool`

--- src/lib/server/db/conn_node-postgres.ts
+++ src/lib/server/db/conn_node-postgres.ts
@@ -1,10 +1,10 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import pg from 'pg';
 
 import { env } from '$env/dynamic/private';
 
 // const connectionString = env.POSTGRES_URL + '?sslmode=require'; // Vercel Postgres
 const connectionString = env.DATABASE_URL + '?sslmode=require'; // Neon
 
-export const pool = new Pool({ connectionString });
+export const pool = new pg.Pool({ connectionString });
 export const conn = drizzle(pool);
