b28686a7f4cf2adff93bbb83fd23a12b02c6ac9d
Sean Lynch <techniq35@gmail.com>
2023-05-07 19:32:30 -0400

    Still need to setup `neonConfig.webSocketConstructor` on non-edge hosting

--- src/lib/server/db/conn.ts
+++ src/lib/server/db/conn.ts
@@ -2,13 +2,11 @@ import { drizzle } from 'drizzle-orm/neon-serverless';
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
 
-import { dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
 
-if (dev) {
-	// https://github.com/neondatabase/serverless#run-on-node
-	neonConfig.webSocketConstructor = ws;
-}
+// TODO: Do not set when van on edge
+// https://github.com/neondatabase/serverless#run-on-node
+neonConfig.webSocketConstructor = ws;
 
 // const connectionString = env.POSTGRES_URL; // Vercel Postgres
 const connectionString = env.DATABASE_URL; // Neon
