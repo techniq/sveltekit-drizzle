2e47aab7d02eb1b54d0260357d38d2179711e374
Sean Lynch <techniq35@gmail.com>
2023-05-07 19:40:21 -0400

    Switch from neondatabase/serverless (WebSocket) to node-postgres (TCP) since not using edge at the moment

--- src/lib/server/db/conn.ts
+++ src/lib/server/db/conn.ts
@@ -1,15 +1,10 @@
-import { drizzle } from 'drizzle-orm/neon-serverless';
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import ws from 'ws';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
 import { env } from '$env/dynamic/private';
 
-// TODO: Do not set when van on edge
-// https://github.com/neondatabase/serverless#run-on-node
-neonConfig.webSocketConstructor = ws;
-
-// const connectionString = env.POSTGRES_URL; // Vercel Postgres
-const connectionString = env.DATABASE_URL; // Neon
+// const connectionString = env.POSTGRES_URL + '?sslmode=require'; // Vercel Postgres
+const connectionString = env.DATABASE_URL + '?sslmode=require'; // Neon
 
 export const pool = new Pool({ connectionString });
 export const conn = drizzle(pool);
