eee8a45baf7dfe13e4a9819d13cfdc7f152df2ed
Sean Lynch <techniq35@gmail.com>
2023-05-08 23:21:21 -0400

    Begin setting up sveltekit-superforms, zod, and drizzle-zod

--- src/routes/auth/register/+page.server.ts
+++ src/routes/auth/register/+page.server.ts
@@ -1,39 +1,62 @@
 import { fail, redirect } from '@sveltejs/kit';
 import { auth } from '$lib/server/auth';
+import { z } from 'zod';
+import { message, superValidate } from 'sveltekit-superforms/server';
+import { createInsertSchema } from 'drizzle-zod';
+import { User } from '$lib/server/db/schema';
 
-// Redirect user if already signed in
+// const userSchema = createInsertSchema(User);
+
+const userSchema = z.object({
+	firstName: z.string().nonempty(),
+	lastName: z.string().nonempty(),
+	email: z.string().email(),
+	username: z.string().min(4),
+	password: z.string().min(6)
+});
+
 export async function load({ locals }) {
+	// Redirect user if already signed in
 	const session = await locals.auth.validate();
 	if (session) throw redirect(302, '/');
-	return {};
+
+	const form = await superValidate(userSchema);
+	return { form };
 }
 
 export const actions = {
 	default: async ({ request, locals }) => {
-		const form = await request.formData();
-		const username = form.get('username');
-		const password = form.get('password');
+		const form = await superValidate(request, userSchema);
 
-		// check for empty values
-		if (typeof username !== 'string' || typeof password !== 'string') {
-			return fail(400);
+		console.log(form);
+
+		if (!form.valid) {
+			return fail(400, { form });
 		}
 
 		try {
 			const user = await auth.createUser({
 				primaryKey: {
 					providerId: 'username',
-					providerUserId: username,
-					password
+					providerUserId: form.data.username,
+					password: form.data.password
 				},
 				attributes: {
-					username
+					username: form.data.username,
+					first_name: form.data.firstName,
+					last_name: form.data.lastName,
+					email: form.data.email
 				}
 			});
 			const session = await auth.createSession(user.id);
 			locals.auth.setSession(session);
+
+			console.log('user created');
+			message(form, 'User created!');
 		} catch {
 			// username already in use
+			console.log('User already exists!');
+			message(form, 'User already exists!');
 			return fail(400);
 		}
 	}
--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -1,14 +1,55 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import { superForm } from 'sveltekit-superforms/client';
+
+	export let data;
+
+	const { form, errors, message, enhance, delayed, submitting } = superForm(data.form, {
+		resetForm: true
+	});
+
+	$: console.log($errors, $message);
 </script>
+
+{#if $message}
+	<h4>{$message}</h4>
+{/if}
 
 <div>
 	<h1>Create an account</h1>
 	<form method="POST" use:enhance>
+		<label for="firstName">First name</label><br />
+		<input id="firstName" name="firstName" bind:value={$form.firstName} /><br />
+		{#if $errors.firstName}
+			<small style:display="block">{$errors.firstName}</small>
+		{/if}
+
+		<label for="lastName">Last name</label><br />
+		<input id="lastName" name="lastName" bind:value={$form.lastName} /><br />
+		{#if $errors.lastName}
+			<small style:display="block">{$errors.lastName}</small>
+		{/if}
+
+		<label for="email">Email</label><br />
+		<input id="email" name="email" bind:value={$form.email} /><br />
+		{#if $errors.email}
+			<small style:display="block">{$errors.email}</small>
+		{/if}
+
 		<label for="username">Username</label><br />
-		<input id="username" name="username" /><br />
+		<input id="username" name="username" bind:value={$form.username} /><br />
+		{#if $errors.username}
+			<small style:display="block">{$errors.username}</small>
+		{/if}
+
 		<label for="password">Password</label><br />
-		<input type="password" id="password" name="password" /><br />
+		<input type="password" id="password" name="password" bind:value={$form.password} /><br />
+		{#if $errors.password}
+			<small style:display="block">{$errors.password}</small>
+		{/if}
+
 		<input type="submit" value="Signup" />
+		{#if $submitting}
+			submitting...
+		{/if}
 	</form>
 </div>
